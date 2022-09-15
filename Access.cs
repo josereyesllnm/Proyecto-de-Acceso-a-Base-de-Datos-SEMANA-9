0aa0f6d030dd3ef6a959b048697b58ed84145e22
Gerson Interiano <interianojonathan75@gmail.com>
2022-09-15 13:52:16 -0600

    agrege un nuevo formulario que servira para el inicio de sesion de access

---
+++ Access.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_de_Acceso_a_Base_de_Datos_Access
+{
+    public partial class Access : Form
+    {
+        public Access()
+        {
+            InitializeComponent();
+        }
+    }
+}
