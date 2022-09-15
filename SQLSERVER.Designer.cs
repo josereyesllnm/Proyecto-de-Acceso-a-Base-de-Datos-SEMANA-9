158c83bac52d2d94150744fde57ca8489192a9dd
AlexRomero <AlexRomero@DESKTOP-NFDMETJ>
2022-09-15 15:22:38 -0500

    Cambie el titulo del el formulario de SQLSERVER

--- SQLSERVER.Designer.cs
+++ SQLSERVER.Designer.cs
@@ -30,6 +30,7 @@ namespace Proyecto_de_Acceso_a_Base_de_Datos_Access
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -41,11 +42,22 @@ namespace Proyecto_de_Acceso_a_Base_de_Datos_Access
             this.label1.Size = new System.Drawing.Size(0, 24);
             this.label1.TabIndex = 1;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(85, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(511, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "BIENVENIDO AL INICIO DE SESION DE SQLSERVER";
+            // 
             // SQLSERVER
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(685, 250);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "SQLSERVER";
             this.Text = "SQLSERVER";
@@ -57,5 +69,6 @@ namespace Proyecto_de_Acceso_a_Base_de_Datos_Access
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
