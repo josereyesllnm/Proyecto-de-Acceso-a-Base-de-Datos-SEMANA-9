52b3a3a3fe10ed4613f54bdf26ec838dd3f888e2
Maydelin Guevara <Maydelin Guevara@MAYDELIN>
2022-09-22 10:40:40 -0600

    Edite y agregue propiedades al formulario de reciclaje.

--- Formulario MDI/RECICLAJE.Designer.cs
+++ Formulario MDI/RECICLAJE.Designer.cs
@@ -30,7 +30,33 @@ namespace Formulario_MDI
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RECICLAJE));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(315, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(274, 28);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "\"FORMAS DE RECICLAR\"";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(140, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(573, 190);
+            this.label2.TabIndex = 5;
+            this.label2.Text = resources.GetString("label2.Text");
             // 
             // RECICLAJE
             // 
@@ -39,15 +65,21 @@ namespace Formulario_MDI
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.DoubleBuffered = true;
             this.Name = "RECICLAJE";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "RECICLAJE";
             this.Load += new System.EventHandler(this.RECICLAJE_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
