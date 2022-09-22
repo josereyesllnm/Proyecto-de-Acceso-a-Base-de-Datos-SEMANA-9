ca469abeb40b54d3992762e564752e1d3512d02e
Maydelin Guevara <Maydelin Guevara@MAYDELIN>
2022-09-22 10:23:42 -0600

    AGREGUE PROPIEDADES AL FORMULARIO AHORRO DE ENERGIA.

--- Formulario MDI/AHORRO DE ENERGIA.Designer.cs
+++ Formulario MDI/AHORRO DE ENERGIA.Designer.cs
@@ -30,7 +30,33 @@ namespace Formulario_MDI
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AHORRO_DE_ENERGIA));
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(116, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(614, 198);
+            this.label2.TabIndex = 2;
+            this.label2.Text = resources.GetString("label2.Text");
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(217, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(383, 28);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "\"FORMAS DE AHORRAR ENERGIA\"";
             // 
             // AHORRO_DE_ENERGIA
             // 
@@ -39,15 +65,21 @@ namespace Formulario_MDI
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
             this.DoubleBuffered = true;
             this.Name = "AHORRO_DE_ENERGIA";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "AHORRO DE ENERGIA";
             this.Load += new System.EventHandler(this.AHORRO_DE_ENERGIA_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
